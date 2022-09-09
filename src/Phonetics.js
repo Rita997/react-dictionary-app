5704c413dcc02d899c1aad6c2e461b15ec97c7ba
Rita997 <106235470+Rita997@users.noreply.github.com>
2022-09-09 09:52:28 +0200

    updated the app

---
+++ src/Phonetics.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Phonetics.css";
+
+export default function Phonetics(props) {
+  return (
+    <div className="Phonetics">
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+
+      {props.phonetics.text}
+    </div>
+  );
+}
